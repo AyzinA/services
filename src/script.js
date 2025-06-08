65cf5aa48040821ff0388845c6c86a56c34c5da2
Alexander Ayzin <alexander.ayzin@icloud.com>
2025-06-08 20:27:53 +0300

    online

--- src/script.js
+++ src/script.js
@@ -11,27 +11,30 @@ window.VANTA.WAVES({
 });
 
 
+const GITHUB_BASE_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/services/";
+
 async function loadServices() {
-    const container = document.getElementById("services-container");
+  const container = document.getElementById("services-container");
 
-    const files = await fetch('/services/index.json').then(res => res.json());
+  const files = await fetch(`${GITHUB_BASE_URL}index.json`).then(res => res.json());
 
-    for (const file of files) {
-        const data = await fetch(`/services/${file}`).then(res => res.json());
+  for (const file of files) {
+    const data = await fetch(`${GITHUB_BASE_URL}${file}`).then(res => res.json());
 
-        const card = document.createElement("div");
-        card.className = "service-card";
-        card.innerHTML = `
-        <img src="${data.logo}" alt="${data.name} Logo" />
-        <h2>${data.name}</h2>
-        <div class="service-info">
-            <h3>${data.name}</h3>
-            <p>${data.description}</p>
-        </div>
-        `;
-        card.onclick = () => window.location.href = data.url;
-        container.appendChild(card);
-    }
+    const card = document.createElement("div");
+    card.className = "service-card";
+    card.innerHTML = `
+      <img src="${data.logo}" alt="${data.name} Logo" />
+      <h3>${data.name}</h3>
+      <div class="service-info">
+        <h4>${data.name}</h4>
+        <p>${data.description}</p>
+      </div>
+    `;
+    card.onclick = () => window.open(data.url, "_blank");
+    container.appendChild(card);
+  }
 }
+
 
 loadServices();
