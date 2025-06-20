cfffa5cf6bbdf2e9fb6c9c63e8e0fb6aee50c6b3
Alexander Ayzin <alexander.ayzin@icloud.com>
2025-06-20 20:12:09 +0300

    asd

--- src/script.js
+++ src/script.js
@@ -1,30 +1,30 @@
 window.VANTA.WAVES({
-    el: "#vanta-bg",
-    mouseControls: false,
-    touchControls: false,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x21222
+  el: "#vanta-bg",
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x21222
 });
 
 
-const GITHUB_BASE_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/services/";
-
+const GITHUB_SERVICES_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/services/";
+const GITHUB_PUBLIC_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/public/"
 async function loadServices() {
   const container = document.getElementById("services-container");
 
-  const files = await fetch(`${GITHUB_BASE_URL}index.json`).then(res => res.json());
+  const files = await fetch(`${GITHUB_SERVICES_URL}index.json`).then(res => res.json());
 
   for (const file of files) {
-    const data = await fetch(`${GITHUB_BASE_URL}${file}.json`).then(res => res.json());
+    const data = await fetch(`${GITHUB_SERVICES_URL}${file}.json`).then(res => res.json());
 
     const card = document.createElement("div");
     card.className = "service-card";
     card.innerHTML = `
-      <img src="${data.logo}" alt="${data.name} Logo" />
+      <img src="${GITHUB_PUBLIC_URL}${data.logo}" alt="${data.name} Logo" />
       <h3>${data.name}</h3>
       <div class="service-info">
         <h4>${data.name}</h4>
