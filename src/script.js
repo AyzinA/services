e04ea309e62ad10025f1c9b6d82172a51a0ae915
Alexander Ayzin <alexander.ayzin@icloud.com>
2025-06-28 13:56:50 +0300

    testa

--- src/script.js
+++ src/script.js
@@ -11,30 +11,36 @@ window.VANTA.WAVES({
 });
 
 
-const GITHUB_SERVICES_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/services/";
-const GITHUB_PUBLIC_URL = "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/public/"
+const GITHUB_BASE =
+  "https://raw.githubusercontent.com/AyzinA/services/refs/heads/master/";
+const SERVICES_JSON = `${GITHUB_BASE}services/services.json`;
+const PUBLIC_ASSETS = `${GITHUB_BASE}public/`;
+
 async function loadServices() {
   const container = document.getElementById("services-container");
 
-  const files = await fetch(`${GITHUB_SERVICES_URL}index.json`).then(res => res.json());
+  // one request instead of many
+  const services = await fetch(SERVICES_JSON).then(res => res.json());
 
-  for (const file of files) {
-    const data = await fetch(`${GITHUB_SERVICES_URL}${file}.json`).then(res => res.json());
+  services
+    .filter(svc => !svc.hide)              // ignore hidden ones
+    .forEach(svc => {
+      const card = document.createElement("div");
+      card.className = "service-card";
 
-    const card = document.createElement("div");
-    card.className = "service-card";
-    card.innerHTML = `
-      <img src="${GITHUB_PUBLIC_URL}${data.logo}" alt="${data.name} Logo" />
-      <h3>${data.name}</h3>
-      <div class="service-info">
-        <h4>${data.name}</h4>
-        <p>${data.description}</p>
-      </div>
-    `;
-    card.onclick = () => window.open(data.url, "_blank");
-    container.appendChild(card);
-  }
+      card.innerHTML = `
+        <img src="${PUBLIC_ASSETS}${svc.logo}" alt="${svc.name} Logo" />
+        <h3>${svc.name}</h3>
+        <div class="service-info">
+          <h4>${svc.name}</h4>
+          <p>${svc.description}</p>
+          <p>${svc.version}</p>
+        </div>
+      `;
+
+      card.onclick = () => window.open(svc.url, "_blank");
+      container.appendChild(card);
+    });
 }
-
 
 loadServices();
