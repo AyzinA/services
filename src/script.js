7344a9c25ce14521755e3a847f2342f832a7aafe
Alexander Ayzin <alexander.ayzin@icloud.com>
2025-06-28 14:09:16 +0300

    asd

--- src/script.js
+++ src/script.js
@@ -19,7 +19,6 @@ const PUBLIC_ASSETS = `${GITHUB_BASE}public/`;
 async function loadServices() {
   const container = document.getElementById("services-container");
 
-  // one request instead of many
   const services = await fetch(SERVICES_JSON).then(res => res.json());
 
   services
@@ -28,13 +27,16 @@ async function loadServices() {
       const card = document.createElement("div");
       card.className = "service-card";
 
+      // only show version if it exists
+      const versionHTML = svc.version ? `<br><p>Version: ${svc.version}</p>` : "";
+
       card.innerHTML = `
         <img src="${PUBLIC_ASSETS}${svc.logo}" alt="${svc.name} Logo" />
         <h3>${svc.name}</h3>
         <div class="service-info">
           <h4>${svc.name}</h4>
           <p>${svc.description}</p>
-          <p>${svc.version}</p>
+          ${versionHTML}
         </div>
       `;
 
@@ -44,3 +46,4 @@ async function loadServices() {
 }
 
 loadServices();
+
